8a91c8c0ef3efa4775d2276d98db5dc11670014a
Sabbir Ahmed <sabbir.ahmed.dev@gmail.com>
2022-11-02 15:24:13 +0600

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { PayPalScriptProvider } from "@paypal/react-paypal-js";
+import Checkout from "./components/Checkout";
+
+import "./styles.css";
+
+export default function App() {
+  return (
+    <PayPalScriptProvider
+      options={{ "client-id": process.env.REACT_APP_PAYPAL_CLIENT_ID }}
+    >
+      <div className="App">
+        <Router>
+          <Routes>
+            <Route index element={<Checkout />} />
+          </Routes>
+        </Router>
+      </div>
+    </PayPalScriptProvider>
+  );
+}
